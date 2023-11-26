5089aedbf8d1958a6e9b91d1f2ae4178bed3147b
MARCO <paulmarquez95@gmail.com>
2023-11-26 09:04:00 -0500

    Clase Bicicleta

---
+++ src/main/java/com/mycompany/impactoecologico/Bicicleta.java
@@ -0,0 +1,25 @@
+public class Bicicleta implements ImpactoEcologico {
+    private double emisionesProduccion;
+    private String tipoBateria; // Solo para bicicletas eléctricas
+    private double eficienciaEnergia;
+    private double mantenimientoAnual;
+    private int vidaUtil;
+    private double impactoReciclaje;
+
+    public Bicicleta(double emisionesProduccion, String tipoBateria, double eficienciaEnergia, double mantenimientoAnual, int vidaUtil, double impactoReciclaje) {
+        this.emisionesProduccion = emisionesProduccion;
+        this.tipoBateria = tipoBateria;
+        this.eficienciaEnergia = eficienciaEnergia;
+        this.mantenimientoAnual = mantenimientoAnual;
+        this.vidaUtil = vidaUtil;
+        this.impactoReciclaje = impactoReciclaje;
+    }
+
+    @Override
+    public double calcularImpactoEcologico() {
+
+        return emisionesProduccion + (eficienciaEnergia * mantenimientoAnual) + (impactoReciclaje / vidaUtil);
+    }
+
+}
+
